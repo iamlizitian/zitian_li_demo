782331888668be26ccf492a49a95c9c48b4eda91
iamlizitian <iamlizitian@gmail.com>
2024-03-06 11:12:33 -0600

    yes

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ This example uses p5 preload function to create the classifier
 //https://teachablemachine.withgoogle.com/models/c5yUTMFy5/
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+let imageModelURL = 'c';
 
 // Video
 let video;
